02f871fae3d20931cafcbbc5512b263b41b93fcb
pratham yadav <ypratham411@gmail.com>
2025-03-22 15:03:13 +0530

    UI checked

--- src/cnnClassifier/pipeline/predict.py
+++ src/cnnClassifier/pipeline/predict.py
@@ -1,31 +1,33 @@
 import numpy as np
-import tensorflow
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import os
 
-
-
 class PredictionPipeline:
-    def __init__(self,filename):
-        self.filename =filename
+    def __init__(self, filename):
+        self.filename = filename
 
-
-    
     def predict(self):
-        # load model
-        model = load_model(os.path.join("artifacts","training", "model.h5"))
+        # Load the trained model
+        model = load_model(os.path.join("artifacts", "training", "model.h5"))
 
+        # Preprocess input image
         imagename = self.filename
-        test_image = image.load_img(imagename, target_size = (224,224))
+        test_image = image.load_img(imagename, target_size=(224, 224))
         test_image = image.img_to_array(test_image)
-        test_image = np.expand_dims(test_image, axis = 0)
-        result = np.argmax(model.predict(test_image), axis=1)
-        print(result)
+        test_image = np.expand_dims(test_image, axis=0)
+        test_image = test_image / 255.0  # Normalize if not already done in model
 
-        if result[0] == 1:
-            prediction = 'Healthy'
-            return [{ "image" : prediction}]
-        else:
-            prediction = 'Coccidiosis'
-            return [{ "image" : prediction}]
+        # Predict class
+        prediction_idx = np.argmax(model.predict(test_image), axis=1)[0]
+        
+        # Map prediction index to class name
+        class_labels = {
+            0: "NonDemented",
+            1: "MildDemented",
+            2: "VeryMildDemented",
+            3: "ModerateDemented"
+        }
+        
+        prediction = class_labels.get(prediction_idx, "Unknown")
+        return [{"image": prediction}]
